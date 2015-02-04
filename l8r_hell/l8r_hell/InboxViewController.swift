72d0ae72d60303046cf55063b8d7ac6070a042fb
nsbarr <nsbarr@gmail.com>
2015-02-03 20:06:02 -0500

    doin work

--- l8r_hell/l8r_hell/InboxViewController.swift
+++ l8r_hell/l8r_hell/InboxViewController.swift
@@ -9,18 +9,25 @@
 import Foundation
 import UIKit
 
-class InboxViewController: UIViewController {
+class InboxViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBOutlet var cameraButton: UIButton!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var datePicker: UIDatePicker!
     
-    @IBOutlet var theImageView: UIImageView!
+    var imageToShow: UIImage!
+    var cyaButton: UIButton!
+    var calButton: UIButton!
+    var scheduleButton: UIButton!
+    
+    
+    let circleViewFrameSize:CGSize = CGSizeMake(80, 80)
+    let actionButtonWidth:CGFloat = 60
     
     
     @IBAction func cameraButtonTapped(sender: UIButton){
         
         let cvc = self.storyboard!.instantiateViewControllerWithIdentifier("CameraViewController") as CameraViewController
-        //   ivc.modalTransitionStyle = UIModalTransitionStyle.CrossDissolve
-        // pvc.setViewControllers([ivc], direction: UIPageViewControllerNavigationDirection.Reverse, animated: true, completion: nil)
         self.presentViewController(cvc, animated: true, completion: nil)
         
     }
@@ -29,27 +36,160 @@ class InboxViewController: UIViewController {
         super.viewDidLoad()
         println("inboxViewLoaded")
         
-        
         let theImagePath = NSUserDefaults.standardUserDefaults().objectForKey("imagePath") as NSString
-        theImageView.frame = CGRectMake(0,83,320,568)
-        let imageToShow = UIImage(contentsOfFile: theImagePath)
-        let stretchRatio = 1.333333
-        println(stretchRatio)
-        
-      //  theImageView.image = imageToShow
+        imageToShow = UIImage(contentsOfFile: theImagePath)
+        if imageToShow != nil { //load inbox
+            self.addActionButtons()
+            self.setUpInbox()
+        }
+        else {
+            println("no image to show")
+        }
+    
+        datePicker.hidden = true
+        datePicker.frame = self.view.frame
+        datePicker.center = self.view.center
 
-        theImageView.image = imageWithImage(imageToShow!, scaledToSize: CGSizeMake(theImageView.frame.size.width,theImageView.frame.size.height))
-        
-//        var newFrame = theImageView.frame
-//        newFrame.origin.y = view.frame.height-140
-//        theImageView.frame = newFrame
-        
-
-        view.addSubview(theImageView)
         view.addSubview(cameraButton)
         view.bringSubviewToFront(cameraButton)
+        println(imageView.superview)
        
     }
+    
+    func setUpInbox(){
+        
+
+        
+     //   imageView.frame = CGRectMake(0,83,320,568)
+     //   let stretchRatio = 1.333333
+        imageView.image = imageToShow
+    //    imageView.image = imageWithImage(imageToShow!, scaledToSize: CGSizeMake(imageView.frame.size.width,imageView.frame.size.height))
+        imageView.alpha = 1
+        imageView.layer.cornerRadius = 0
+     //   view.addSubview(imageView)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: Selector("inboxImageWasTapped:"))
+        imageView.userInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGesture)
+
+    }
+    
+    func inboxImageWasTapped(sender: UITapGestureRecognizer){
+        println("tap")
+        sender.delegate = self
+        
+        imageView.layer.cornerRadius = circleViewFrameSize.width/2
+        imageView.clipsToBounds = true
+        
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            
+            self.imageView.frame.size = self.circleViewFrameSize
+            self.imageView.center = self.view.center
+            //    self.imageView.center = self.pointInView
+            
+            }, completion:nil)
+    }
+        
+
+    
+    func addActionButtons(){
+        
+        if cyaButton != nil {
+            cyaButton.removeFromSuperview()
+        }
+        
+        if calButton != nil {
+            calButton.removeFromSuperview()
+        }
+        
+        if scheduleButton != nil {
+            scheduleButton.removeFromSuperview()
+        }
+        
+        cyaButton = UIButton(frame: CGRectMake(20,self.view.frame.height-60, actionButtonWidth, actionButtonWidth))
+        cyaButton.addTarget(self, action: Selector("trashImage:"), forControlEvents:UIControlEvents.TouchUpInside)
+        let cyaButtonImage = UIImage(named: "cyaButtonImage")
+        cyaButton.setImage(cyaButtonImage, forState: .Normal)
+        cyaButton.alpha = 1
+        view.addSubview(cyaButton)
+        view.sendSubviewToBack(cyaButton)
+        
+        calButton = UIButton(frame: CGRectMake(self.view.frame.width-70,self.view.frame.height-60, actionButtonWidth, actionButtonWidth))
+        calButton.addTarget(self, action: Selector("openCalendar:"), forControlEvents:UIControlEvents.TouchUpInside)
+        let calButtonImage = UIImage(named: "calButtonImage")
+        calButton.setImage(calButtonImage, forState: .Normal)
+        calButton.alpha = 1
+        view.addSubview(calButton)
+        
+        scheduleButton = UIButton(frame: CGRectMake(self.view.frame.width/2,self.view.frame.height-100, actionButtonWidth,actionButtonWidth))
+        scheduleButton.center.x = self.view.center.x
+        scheduleButton.addTarget(self, action: Selector("scheduleImage:"), forControlEvents: UIControlEvents.TouchUpInside)
+        scheduleButton.setTitle("Ok", forState: UIControlState.Normal)
+        scheduleButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        scheduleButton.hidden = true
+        view.addSubview(scheduleButton)
+    }
+    
+    func trashImage(sender: UIButton){
+        
+    }
+    
+    func openCalendar(sender: UIButton){
+        
+        //animate up imageView
+        
+        var imageViewNewFrame = imageView.frame
+        imageViewNewFrame.origin.y = imageView.frame.origin.y-200
+        calButton.hidden = true
+        cyaButton.hidden = true
+        self.scheduleButton.hidden = false
+        
+        UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+            
+            self.imageView.frame = imageViewNewFrame
+            self.datePicker.hidden = false
+            
+            }, completion: {(value: Bool) in
+                println("complete")
+                
+                //add confirmButton
+                
+
+                
+        })
+        
+
+        
+    }
+    
+    func scheduleImage(sender: UIButton){
+        datePicker.hidden = true
+        self.disappearImage()
+        //show next image in the inbox, if there is one.
+        println("scheduled")
+    }
+    
+    func disappearImage(){
+        var imageViewNewFrame = imageView.frame
+        imageViewNewFrame.origin.y = imageView.frame.origin.y-800
+        
+        UIView.animateWithDuration(0.6, delay: 0.2, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
+            
+            self.imageView.alpha = 0.2
+            self.imageView.frame = imageViewNewFrame
+            
+            }, completion: {(value: Bool) in
+                println("complete")
+                
+                //add confirmButton
+
+                self.setUpInbox()
+                self.addActionButtons()
+                
+        })
+
+    }
+    
     func imageWithImage(image:UIImage, scaledToSize newSize:CGSize) -> UIImage{
         UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
         image.drawInRect(CGRectMake(0, 0, newSize.width, newSize.height))
